794f40aa34befbdf27f056a3d9b62c0271ee7c34
Sergey Berezhnoy <serg.berezhnoy@gmail.com>
2013-12-07 22:59:10 +0100

    Removing unnecessary comment

--- src/main/java/opn/noteservice/error/CustomErrorController.java
+++ src/main/java/opn/noteservice/error/CustomErrorController.java
@@ -23,7 +23,6 @@ class CustomErrorController {
 		// retrieve some useful information from the request
 		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
 		Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
-		// String servletName = (String) request.getAttribute("javax.servlet.error.servlet_name");
 		String exceptionMessage = getExceptionMessage(throwable, statusCode);
 		
 		String requestUri = (String) request.getAttribute("javax.servlet.error.request_uri");
